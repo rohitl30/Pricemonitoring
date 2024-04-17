feb5f07de5af2716457b39c487d1d34660e76e50
rohitl30 <rohit.luniya619@gmail.com>
2024-04-17 23:44:54 +0530

    Add basic Price monitoring

---
+++ app.py
@@ -0,0 +1,106 @@
+from flask import Flask, render_template, request, redirect, url_for, jsonify
+from selenium import webdriver
+from bs4 import BeautifulSoup
+from pymongo import MongoClient
+
+app = Flask(__name__)
+
+client = MongoClient('mongodb://localhost:27017/')
+db = client['pricemonitoring']
+users_collection = db['users']
+products_collection = db['products']
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/login', methods=['POST'])
+def login():
+    username = request.form['username']
+    password = request.form['password']
+
+    users = users_collection.find()
+    for user in users:
+        if user['username']==username and user['password']==password : 
+            return redirect(url_for('monitoring'))
+    
+    message = "Invalid Credentials! Try Again!"
+    return render_template('index.html', message=message)
+
+@app.route('/logout')
+def logout():
+    return redirect(url_for('index'))
+
+
+@app.route('/monitoring')
+def monitoring():
+    products = products_collection.find()
+    return render_template('monitoring.html',products=products)
+
+@app.route('/ebay')
+def get_ebay_price():
+    url="https://www.ebay.com/itm/256458485567"
+    options = webdriver.ChromeOptions()
+    options.add_argument('--headless')  
+    driver = webdriver.Chrome(options=options)
+
+    driver.get(url)
+    html = driver.page_source
+    driver.quit()  
+    soup = BeautifulSoup(html, 'html.parser')
+
+    price_element = soup.find("div", {"class": "x-price-primary"})
+    if price_element:
+        return jsonify({'ebay_price': price_element.text})
+    else:
+        return "NaN"
+    
+@app.route('/amazon')
+def get_amazon_price():
+    url = "https://www.amazon.com/Epson-DURABrite-Ultra-Capacity-Cartridge/dp/B08DX5F6XV"
+    options = webdriver.ChromeOptions()
+    options.add_argument('--headless')  
+    driver = webdriver.Chrome(options=options)
+
+    driver.get(url)
+    html = driver.page_source
+    driver.quit() 
+    soup = BeautifulSoup(html, 'html.parser')
+
+    price_element = soup.find("span", {"id": "style_name_0_price"})
+    if price_element:
+        price_text = price_element.find("span", {"class": "a-size-mini olpWrapper"}).text.strip()
+        amazon_price = price_text.split("from ")[1].split(" ")[0]
+        return jsonify({'amazon_price': amazon_price})
+    else:
+        return "NaN"
+
+@app.route('/cityblue')
+def get_citybluetechnologies_price():
+    product_url ="https://citybluetechnologies.com/product/t812-exra-high-capacity-black-ink-cartridge-sensormatic/"
+
+    options = webdriver.ChromeOptions()
+    options.add_argument('--headless')  
+    driver = webdriver.Chrome(options=options)
+
+    driver.get(product_url)
+    html = driver.page_source
+    driver.quit()  
+    soup = BeautifulSoup(html, 'html.parser')
+
+    price_element = soup.find("span", {"class": "woocommerce-Price-amount amount"})
+    if price_element:
+        return jsonify({'cityblue_price':price_element.text.strip()})
+    else:
+        return "NaN"
+
+@app.route('/update_price', methods=['POST'])
+def update_price():
+    product_id = request.json['product_id']
+    new_price = request.json['new_price']
+    db.products.update_one({'id': product_id}, {'$set': {'updated_cityblue': new_price}})
+    return {"data":"Updated"}
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
